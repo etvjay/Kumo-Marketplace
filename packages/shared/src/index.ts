549a6b2787ea51c8ff8b9c0d595999757618d433
etvjay <etvictory620@gmail.com>
2026-08-24 18:11:56 +0100

    feat: export judgment contracts

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./category.js";
 export * from "./routing.js";
 export * from "./track.js";
+export * from "./judgment.js";
 export * from "./agent.js";
 export * from "./task.js";
 export * from "./activation.js";
